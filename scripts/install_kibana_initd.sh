14e5d94a705a159746d132bfd2fe00c1e5273faf
GavrilS <gavr1lll@abv.bg>
2024-12-08 22:26:05 +0200

    Adding the option to specify which version of kibana to install

--- scripts/install_kibana_initd.sh
+++ scripts/install_kibana_initd.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Install Kibana
-apt install kibana
+# Install a specific version
+while getopts v: value
+do
+    case "${value}"
+        in
+        v)version=${OPTARG};;
+    esac
+done
+echo "$version"
+if [[ ! -z "$version" ]]
+then
+    apt install kibana=$version
+else
+    apt install kibana
+fi
 
 # Start Kibana service
 /etc/init.d/kibana start
